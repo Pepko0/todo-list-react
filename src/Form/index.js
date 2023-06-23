5b3a87d7abc0563b1603bab4e5adbce3a5a044ff
Kacper Lewko <k.lewkosga@gmail.com>
2023-06-23 15:37:45 +0200

    added focus to input NewTask

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Body, NewTask, Button } from "./styled";
 
 const Form = ({ addNewTask }) => {
     const [newTaskContent, setNewTaskContent] = useState("");
+    const inputRef = useRef(null);
 
     const onFormSubmit = (event) => {
         event.preventDefault();
@@ -12,16 +13,22 @@ const Form = ({ addNewTask }) => {
         };
     };
 
+    const focusInput = () => {
+        inputRef.current.focus();
+    }
+
     return (
         <Body
             onSubmit={onFormSubmit}
         >
             <NewTask
+                ref={inputRef}
                 value={newTaskContent}
                 placeholder="Co jest do zrobienia?"
                 onChange={({ target }) => setNewTaskContent(target.value)}
             />
             <Button
+                onClick={focusInput}
                 className="form__button"
             >
                 Dodaj zadanie
