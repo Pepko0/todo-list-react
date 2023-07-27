678a68961c2fc5120917c3414ad7454ea9a04dd5
Kacper Lewko <k.lewkosga@gmail.com>
2023-07-27 14:12:01 +0200

    deleted Tasks.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
-import Tasks from "./features/tasks/Tasks";
+import Form from "./features/tasks/Form";
+import TaskList from "./features/tasks/TaskList";
+import Buttons from "./features/tasks/Buttons";
+import Section from "./common/Section";
 
-export default () => <Tasks />;
+export default () => (
+    <main>
+    <h1>Lista zadań</h1>
+    <Section title="Dodaj nowe zadanie" body={<Form />} />
+    <Section
+      title="Lista zadań"
+      body={<TaskList />}
+      extraHeaderContent={<Buttons />}
+    />
+  </main>
+);
