4b354eed6a271a7687b09ecada43fa94e642e65e
Kacper Lewko <k.lewkosga@gmail.com>
2024-02-19 11:58:25 +0100

    added Author link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
+import Author from "./features/tasks/Author";
 
 export default  () => (
   <BrowserRouter>
@@ -9,13 +10,18 @@ export default  () => (
       <li>
         <Link to="/zadania">Zadania</Link>
       </li>
+      <li>
+        <Link to="/autor">Autor</Link>
+      </li>
     </ul>
     <Switch>
       <Route path="/zadania">
         <Tasks />
       </Route>
+      <Route path="/autor">
+        <Author />
+      </Route>
     </Switch>
   </nav>
 </BrowserRouter>
 );
-  
---
+++ src/features/tasks/Author/index.js
@@ -0,0 +1,17 @@
+import Section from "../../../common/Section";
+import Form from "../Form";
+import SampleTaskButton from "../SampleTaskButton";
+
+
+const Author = () => (
+<main>
+<h1>O autorze</h1>
+<Section
+  title="Kacper Lewko" 
+  body = {
+  <>Tralalaa</>
+    }
+  />
+</main>
+);
+export default Author;
